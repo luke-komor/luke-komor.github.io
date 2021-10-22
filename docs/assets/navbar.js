878e61f07fd1ceaee41f18f7775501d64b8acb67
luke <ukasz.komorowski@gmail.com>
2021-10-22 22:18:22 +0200

    Add glow

---
+++ docs/assets/navbar.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("overlay_nav").style.left = "0";
+  }
+  
+  function closeNav() {
+    document.getElementById("overlay_nav").style.left = "100%";
+  }
