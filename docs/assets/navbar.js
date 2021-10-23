f2b16681f7b33b181b00c7cff52ac72734ea697d
luke <ukasz.komorowski@gmail.com>
2021-10-23 22:09:44 +0200

    Add post list

--- docs/assets/navbar.js
+++ docs/assets/navbar.js
@@ -1,7 +1,32 @@
 function openNav() {
-    document.getElementById("overlay_nav").style.left = "0";
+  document.getElementById("overlay_nav").style.left = "0";
+  if (navbar.classList.contains('sticky')) {
+    navbar.classList.add("sticky-z-lower");
+    stickyPrev = true
   }
-  
-  function closeNav() {
-    document.getElementById("overlay_nav").style.left = "100%";
+}
+
+function closeNav() {
+  document.getElementById("overlay_nav").style.left = "100%";
+  if (stickyPrev) {
+    navbar.classList.remove("sticky-z-lower")
   }
+}
+
+function changeNavClass() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+window.onscroll = function () { changeNavClass() };
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+var stickyPrev = false
+
+
+
+
+
